5a22f287a1458e3f607b2869c5afc18a8bf82249
way205-code <buihathieb20052007@gmail.com>
2026-05-20 21:48:55 +0700

    Create vja.java

---
+++ vja.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class vja {public static void main(String[] args) {
+    int sum = 0;
+for (int i = 0; i<=10;i++)
+    sum += i;
+System.out.println("The sum of numbers from 0 to 10 is: " + sum);
+}
+}
+
+
+  
+
