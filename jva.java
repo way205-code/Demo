8b889200cd246390ff5c0652a8ca9bd17da7f856
way205-code <buihathieb20052007@gmail.com>
2026-05-20 21:32:45 +0700

    Create jva.java

--- jva.java
+++ jva.java
@@ -2,6 +2,11 @@ package Demo;
 
 public class jva {public static void main(String[] args) {
     System.out.println("hello world");
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    System.out.println("The sum of a and b is: " + sum);
+
 }
 
     
