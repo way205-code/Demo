c3bb928829c8b1cf7904904b1b0c3e8478186cde
way205-code <buihathieb20052007@gmail.com>
2026-05-25 20:42:03 +0700

    Create btap2

---
+++ btap2.java
@@ -0,0 +1,15 @@
+package Demo;
+import java.util.Scanner;
+public class btap2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("nhap vao so n1: ");
+        int n1 = sc.nextInt();
+        System.out.println("nhap so n2");
+        int n2 = sc.nextInt();
+        sc.nextLine();
+        System.out.println(n1+n2);
+
+
+    }
+}
