47e2e7511115c1af664b4085ef9e82c9232230c3
way205-code <buihathieb20052007@gmail.com>
2026-05-25 21:59:35 +0700

    Create btap4

--- btap4.java
+++ btap4.java
@@ -9,16 +9,19 @@ public class btap4 {public static void main(String[] args) {
         sc.nextLine();
         int temp = 0;
         int sum = 0;
+        int count = 0;
         while (n > 0) {
             temp = n % 10;
             n /= 10;
+            count++;
             sum = sum + temp;
 
-
-            
-
         }
+        
         System.out.println(sum);
+        System.out.println(sum/count);
+
+
 }
 
     
